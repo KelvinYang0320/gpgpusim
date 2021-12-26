c58822256ce42a2a2f7dd2578568c44defe01020
KelvinYang0320 <kelvin777320@gmai.com>
2021-12-26 13:08:40 +0800

    matadd update

--- cuda-example/matadd.cu
+++ cuda-example/matadd.cu
@@ -1,6 +1,7 @@
-#include<stdio.h>
-#include<cuda.h>
-#include<cuda_runtime.h>
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <stdlib.h>
 
 #define N 512
 
@@ -10,13 +11,13 @@ __global__ void MatAdd(float *A, float *B, float *C)
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
     // C = A + B
-    if(i < N && j < N){
-        C[i*N+j] = A[i*N+j] + B[i*N+j];
+    if (i < N && j < N)
+    {
+        C[i * N + j] = A[i * N + j] + B[i * N + j];
     }
-
 }
 
-int main()
+int main(int argc, char *argv[])
 {
 
     float *h_A, *h_B, *h_C;
@@ -24,31 +25,37 @@ int main()
 
     int i;
 
+    int x_dim, y_dim;
+    char *p1;
+    char *p2;
+    x_dim = strtol(argv[1], &p1, 10);
+    y_dim = strtol(argv[2], &p2, 10);
+
     // allocate the host memory
-    h_A = (float*)malloc(N * N * sizeof(float));
-    h_B = (float*)malloc(N * N * sizeof(float));
-    h_C = (float*)malloc(N * N * sizeof(float));
+    h_A = (float *)malloc(N * N * sizeof(float));
+    h_B = (float *)malloc(N * N * sizeof(float));
+    h_C = (float *)malloc(N * N * sizeof(float));
 
     // init host data
-    for(i = 0; i < (N * N); i++)
+    for (i = 0; i < (N * N); i++)
     {
-	    h_A[i] = 1.0;
-	    h_B[i] = 2.0;
-	    h_C[i] = 0.0;
+        h_A[i] = 1.0;
+        h_B[i] = 2.0;
+        h_C[i] = 0.0;
     }
 
     // allocate device memory
-    cudaMalloc((void**)&d_A,  N * N * sizeof(float));
-    cudaMalloc((void**)&d_B,  N * N * sizeof(float));
-    cudaMalloc((void**)&d_C,  N * N * sizeof(float));
-    
+    cudaMalloc((void **)&d_A, N * N * sizeof(float));
+    cudaMalloc((void **)&d_B, N * N * sizeof(float));
+    cudaMalloc((void **)&d_C, N * N * sizeof(float));
+
     // transfer host data to device
     cudaMemcpy(d_A, h_A, N * N * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, N * N * sizeof(float), cudaMemcpyHostToDevice);
 
     // fill in correct thread/blocks
-    unsigned int x_size = 16;
-    unsigned int y_size = 16;
+    unsigned int x_size = x_dim;
+    unsigned int y_size = y_dim;
     unsigned int grid_cols = (N + x_size - 1) / x_size;
     unsigned int grid_rows = (N + y_size - 1) / y_size;
     dim3 dimGrid(grid_cols, grid_rows);
@@ -61,16 +68,16 @@ int main()
     cudaMemcpy(h_C, d_C, N * N * sizeof(float), cudaMemcpyDeviceToHost);
 
     int all_ok = 1;
-    for(i = 0; i < (N * N); i++)
+    for (i = 0; i < (N * N); i++)
     {
-        if(h_C[i]!= 3.0)
+        if (h_C[i] != 3.0)
         {
-	        all_ok = 0;
+            all_ok = 0;
             printf("wrong: %d", i);
-	    }
+        }
     }
 
-    if(all_ok)
+    if (all_ok)
     {
         printf("all results are correct!!!\n");
     }
